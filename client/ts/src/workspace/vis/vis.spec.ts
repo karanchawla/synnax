8096ec8f756eff70afdb534dfa36c6aa3e1047e6
Emiliano Bonilla <56323762+emilbon99@users.noreply.github.com>
2024-04-10 10:45:31 -0500

    [client/ts] - renamed pid to schematic in vis tests

--- client/ts/src/workspace/vis/vis.spec.ts
+++ client/ts/src/workspace/vis/vis.spec.ts
@@ -22,14 +22,14 @@ describe("Vis", () => {
         name: "Schematic",
         layout: { one: 1 },
       });
-      const vis = await client.workspaces.vis.create(ws.key, {
+      const schematic = await client.workspaces.vis.create(ws.key, {
         name: "Schematic",
         type: "schematic",
         data: { one: 1 },
       });
-      expect(vis.name).toEqual("Schematic");
-      expect(vis.key).not.toEqual(ZERO_UUID);
-      expect(vis.data.one).toEqual(1);
+      expect(schematic.name).toEqual("Schematic");
+      expect(schematic.key).not.toEqual(ZERO_UUID);
+      expect(schematic.data.one).toEqual(1);
     });
   });
   describe("rename", () => {
@@ -38,13 +38,13 @@ describe("Vis", () => {
         name: "Schematic",
         layout: { one: 1 },
       });
-      const linePlot = await client.workspaces.vis.create(ws.key, {
+      const schematic = await client.workspaces.vis.create(ws.key, {
         name: "Schematic",
         type: "schematic",
         data: { one: 1 },
       });
-      await client.workspaces.vis.rename(linePlot.key, "Schematic2");
-      const res = await client.workspaces.vis.retrieve(linePlot.key);
+      await client.workspaces.vis.rename(schematic.key, "Schematic2");
+      const res = await client.workspaces.vis.retrieve(schematic.key);
       expect(res.name).toEqual("Schematic2");
     });
   });
@@ -54,13 +54,13 @@ describe("Vis", () => {
         name: "Schematic",
         layout: { one: 1 },
       });
-      const linePlot = await client.workspaces.vis.create(ws.key, {
+      const schematic = await client.workspaces.vis.create(ws.key, {
         name: "Schematic",
         type: "schematic",
         data: { one: 1 },
       });
-      await client.workspaces.vis.setData(linePlot.key, { two: 2 });
-      const res = await client.workspaces.vis.retrieve(linePlot.key);
+      await client.workspaces.vis.setData(schematic.key, { two: 2 });
+      const res = await client.workspaces.vis.retrieve(schematic.key);
       expect(res.data.two).toEqual(2);
     });
   });
@@ -70,13 +70,13 @@ describe("Vis", () => {
         name: "Schematic",
         layout: { one: 1 },
       });
-      const linePlot = await client.workspaces.vis.create(ws.key, {
+      const schematic = await client.workspaces.vis.create(ws.key, {
         name: "Schematic",
         type: "schematic",
         data: { one: 1 },
       });
-      await client.workspaces.vis.delete(linePlot.key);
-      await expect(client.workspaces.vis.retrieve(linePlot.key)).rejects.toThrow();
+      await client.workspaces.vis.delete(schematic.key);
+      await expect(client.workspaces.vis.retrieve(schematic.key)).rejects.toThrow();
     });
   });
 });
